a79f479bf018bdff48b14153f3fa80db9ad98412
lanaharriet <lanaharriet1408@gmail.com>
2026-02-10 02:32:32 +0530

    UI/UX Design

--- community/urls.py
+++ community/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = "community"
+app_name = 'community'
 
 urlpatterns = [
     path('', views.feed, name='feed'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('react/<int:post_id>/<int:value>/', views.react_post, name='react_post'),
     path('delete-post/<int:post_id>/', views.delete_post, name='delete_post'),
     path('delete-comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
-    path('react/<int:post_id>/<int:value>/', views.react_post, name='react_post'),
-    
-
 ]
